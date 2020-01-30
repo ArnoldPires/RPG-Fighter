879b87cc9b73bdc44931015c0b85bea70ce31734
ArnoldPires <arnold.pires92@gmail.com>
2020-01-30 13:13:25 -0500

    added a bunch of css, removed robots png image, fixed some errors, esp in the enemy.js file

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -1,41 +1,41 @@
 let GameManager = {
-  setGameStart: function(classTpye) {
-    this.resetPlayer(classTpye);
+  setGameStart: function(classType) {
+    this.resetPlayer(classType);
     this.setPreFight();
   },
-  resetPlayer: function(classTpye) {
-    switch (classTpye) {
+  resetPlayer: function(classType) {
+    switch (classType) {
       case "elementalist":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 80, 0, 50, 200, 50);
         break;
       case "engineer":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "guardian":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "mesmer":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "necromancer":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "ranger":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 50, 200, 50);
         break;
       case "revenant":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
       case "thief":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 100, 0, 100, 150, 200);
         break;
       case "warrior":
-        player = new Player(classTpye, 200, 0, 200, 100, 50);
+        player = new Player(classType, 200, 0, 200, 100, 50);
         break;
     }
     let getInterface = document.querySelector(".interface");
     getInterface.innerHTML = '<img src="images/heros/' +
-    classTpye.toLowerCase() + '.png" class="images/heros"><div><h3>' + classTpye
+    classType.toLowerCase() + '.png" class="images/heros"><div><h3>' + classType
     + '</h3><p>Health: ' + player.health +
     '</p><p>Mana: ' + player.mana +
     '</p><p>strength: ' + player.strength +
@@ -55,21 +55,20 @@ let GameManager = {
     let getActions = document.querySelector(".actions");
     let getEnemy = document.querySelector(".enemy");
     //Create enemy
-    let enemy00 = new Enemy("centaur, 100, 0, 50, 100, 100");
-    let enemy01 = new Enemy("demon, 100, 0, 50, 100, 100");
-    let enemy02 = new Enemy("djinn, 100, 0, 50, 100, 100");
-    let enemy03 = new Enemy("dragon, 100, 0, 50, 100, 100");
-    let enemy04 = new Enemy("ettin, 100, 0, 50, 100, 100");
-    let enemy05 = new Enemy("grawl, 100, 0, 50, 100, 100");
-    let enemy06 = new Enemy("harpy, 100, 0, 50, 100, 100");
-    let enemy07 = new Enemy("hydra, 100, 0, 50, 100, 100");
-    let enemy08 = new Enemy("jotun, 100, 0, 50, 100, 100");
-    let enemy09 = new Enemy("krait, 100, 0, 50, 100, 100");
-    let enemy10 = new Enemy("orgre, 100, 0, 50, 100, 100");
-    let enemy11 = new Enemy("robot, 100, 0, 50, 100, 100");
-    let enemy12 = new Enemy("troll, 100, 0, 50, 100, 100");
-    let enemy13 = new Enemy("undead, 100, 0, 50, 100, 100");
-    let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(14));
+    let enemy00 = new Enemy("centaur", 100, 0, 50, 100, 10);
+    let enemy01 = new Enemy("demon", 100, 0, 50, 100, 100);
+    let enemy02 = new Enemy("djinn", 100, 0, 50, 100, 100);
+    let enemy03 = new Enemy("dragon", 100, 0, 50, 100, 100);
+    let enemy04 = new Enemy("ettin", 100, 0, 50, 100, 100);
+    let enemy05 = new Enemy("grawl", 100, 0, 50, 100, 100);
+    let enemy06 = new Enemy("harpy", 100, 0, 50, 100, 100);
+    let enemy07 = new Enemy("hydra", 100, 0, 50, 100, 100);
+    let enemy08 = new Enemy("jotun", 100, 0, 50, 100, 100);
+    let enemy09 = new Enemy("krait", 100, 0, 50, 100, 100);
+    let enemy10 = new Enemy("ogre", 100, 0, 50, 100, 100);
+    let enemy11 = new Enemy("troll", 100, 0, 50, 100, 100);
+    let enemy12 = new Enemy("undead", 100, 0, 50, 100, 100);
+    let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(13));
     switch (chooseRandomEnemy) {
       case 0:
         enemy = enemy00;
@@ -110,14 +109,11 @@ let GameManager = {
       case 12:
         enemy = enemy12;
         break;
-      case 13:
-        enemy = enemy13;
-        break;
     }
     getHeader.innerHTML = '<p>Task: Choose your move!</p>';
     getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
     getEnemy.innerHTML = '<img src="images/enemies/' +
-    enemy.enemyType.toLowerCase() + '.png alt="' + enemy.enemyType + '" class="images/enemies"><div><h3>' +
+    enemy.enemyType.toLowerCase() + '.jpg" alt="' + enemy.enemyType + '" class="images/heros"><div><h3>' +
     enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' +
     enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' +
     enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
